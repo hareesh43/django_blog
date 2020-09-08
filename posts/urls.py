338b035b5d529bf0a886e76e61700bae215e4ef0
hareesh43 <hareeshgummani@gmail.com>
2020-09-08 19:47:58 +0530

    articles details routing features

--- posts/urls.py
+++ posts/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('',views.post_list,name = 'posts'),
     path('create/',views.create_post,name = 'create'),
-    path('details/',views.post_details,name= 'details'),
+    path('details/<str:details>/',views.post_details,name= 'details'),
     path('update/',views.post_update,name ='update'),
     path('delete/',views.post_delete,name = 'delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -12,9 +12,10 @@ def create_post(request):
     content = 'create'
     return render(request,'index.html',{'content':content})
     
-def post_details(request):
+def post_details(request,details):
+    object_details = get_object_or_404(Post,title = details)
     content = 'details'
-    return render(request,'index.html',{'content':content})
+    return render(request,'details.html',{'object':object_details,'content':content})
     
 def post_update(request):
     content = 'update'
