d9b661e386102cafcdcb2838bb148c68bd4db266
hareesh43 <hareeshgummani@gmail.com>
2020-09-08 08:28:40 +0530

    custom admin sight

---
+++ posts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['__str__','timestamp','updated']
+    class Meta:
+        model = Post
+        
+
+
+admin.site.register(Post,PostModelAdmin)
